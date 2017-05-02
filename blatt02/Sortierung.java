5b8b281bc749a8da3fb42bc78041834eef2232a9
Feniel <Gamer_World@web.de>
2017-05-02 16:53:12 +0200

    add classes

---
+++ blatt02/Sortierung.java
@@ -0,0 +1,16 @@
+
+public class Sortierung {
+	
+	
+	public static void insertionSort(int[] array){
+		
+	}
+	
+	public static boolean isSorted(int[] array){
+		
+	}
+	
+	public static void main(String[] args){
+		
+	}
+}
